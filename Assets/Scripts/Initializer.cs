88a33492f3a03a2da8ad0ace6d9865b907670042
Eugenio Tavares <eugenio00@gmail.com>
2015-06-15 08:13:37 -0300

    Initial Commit

---
+++ Assets/Scripts/Initializer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Initializer : MonoBehaviour
+{
+    void Start()
+    {
+        if (Camera.main.gameObject.GetComponent<ResolutionCameraPixelPerfect>() == null)
+        {
+            Camera.main.gameObject.AddComponent<ResolutionCameraPixelPerfect>();
+        }
+    }
+}
